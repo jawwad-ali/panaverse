78349bbfc4bd8d6639dcbd74e5be033cd76b9449
jawwad-ali <alijawwad001@gmail.com>
2023-02-21 16:23:59 +0500

    navbar complete

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,127 @@
+"use client";
+import {
+  Flex,
+  ListItem,
+  UnorderedList,
+  Button,
+  Box,
+  IconButton,
+} from "@chakra-ui/react";
+import Link from "next/link";
+import Image from "next/image";
+
+import Logo from "../../assets/panaverse-logo.webp";
+
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+
+import { useState } from "react";
+
+const Navbar = () => {
+  const [display, setDisplay] = useState("none");
+  return (
+    <Flex maxW={"100%"} justifyContent={"space-between"} alignItems={"center"}>
+      <Flex px="8" py={"4"} flex="0.6">
+        <Image src={Logo} alt="logo" height={"100"} width={"100"} />
+      </Flex>
+
+      <Box flex={"0.4"} display={{ base: "none", md: "none", lg: "flex" }}>
+        <UnorderedList
+          listStyleType={"none"}
+          display={"flex"}
+          w="100%"
+          justifyContent={"space-around"}
+          alignItems={"center"}
+        >
+          <ListItem>
+            <Link href="/">Home</Link>
+          </ListItem>
+
+          <ListItem>
+            <Link href="/">Web 3.0</Link>
+          </ListItem>
+
+          <ListItem>
+            <Link href="/">About Us</Link>
+          </ListItem>
+
+          <ListItem>
+            <Link href="/">Courses</Link>
+          </ListItem>
+
+          <ListItem>
+            <Button colorScheme="green">Apply Now</Button>
+          </ListItem>
+        </UnorderedList>
+      </Box>
+
+      {/* Hamburger Icon */}
+      <IconButton
+        aria-label="Open Menu"
+        size="lg"
+        mr="10"
+        icon={<HamburgerIcon />}
+        onClick={() => setDisplay("flex")}
+        display={{ base: "flex", md: "flex", lg: "none" }}
+      />
+
+      {/* Mobile Menu */}
+      <Flex
+        w="100vw"
+        bgColor={"gray.50"}
+        zIndex={50}
+        h="100vh"
+        pos="fixed"
+        top="0"
+        left="0"
+        overflowY="auto"
+        flexDir={"column"}
+        display={display}
+        right="1"
+      >
+        <Flex justify="flex-end">
+          <IconButton
+            mt="2"
+            mr="2"
+            aria-label="Close menu"
+            size="lg"
+            onClick={() => setDisplay("none")}
+            icon={<CloseIcon />}
+          />
+        </Flex>
+
+        <Flex flexDir="column" align="center">
+          <UnorderedList
+            listStyleType={"none"}
+            display={"flex"}
+            flexDir={"column"}
+            w="100%"
+            justifyContent={"space-around"}
+            alignItems={"center"}
+          >
+            <ListItem py="5">
+              <Link href="/">Home</Link>
+            </ListItem>
+
+            <ListItem py="5">
+              <Link href="/">Web 3.0</Link>
+            </ListItem>
+
+            <ListItem py="5">
+              <Link href="/">About Us</Link>
+            </ListItem>
+
+            <ListItem py="5">
+              <Link href="/">Courses</Link>
+            </ListItem>
+
+            <ListItem py="5">
+              <Button colorScheme="green">Apply Now</Button>
+            </ListItem>
+          </UnorderedList>
+        </Flex>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default Navbar;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,20 @@
-import './globals.css'
+import ChakraWrapper from "./components/Chakra";
+import Navbar from "./components/Navbar";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>{children}</body>
+    <html>
+      <head></head>
+      <body>
+        <ChakraWrapper>
+          <Navbar />
+          {children}
+        </ChakraWrapper>
+      </body>
     </html>
-  )
+  );
 }
